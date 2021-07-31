e0f90262728abe55568d9c2a1a8b757a61367fda
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2021-07-31 12:02:25 +0100

    Add files via upload

---
+++ 5-star.py
@@ -0,0 +1,13 @@
+import turtle 
+from turtle import *
+
+def stars():
+    turtle.pensize(3)
+    turtle.speed(5)
+    for i in range(300):
+        circle(190-i,90)
+        left(90)
+        circle(190-i,90)
+        left(200)
+stars()
+mainloop()
