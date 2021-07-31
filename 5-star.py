82b070d94c5e380b82462874792161ef6b8ecb0b
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2021-07-31 12:50:03 +0100

    Update 5-star.py

--- 5-star.py
+++ 5-star.py
@@ -1,13 +1,25 @@
 import turtle 
 from turtle import *
 
-def stars():
-    turtle.pensize(3)
-    turtle.speed(5)
-    for i in range(300):
+# def stars():
+#     turtle.pensize(3)
+#     turtle.speed(5)
+#     for i in range(300):
+#         circle(190-i,90)
+#         left(90)
+#         circle(190-i,90)
+#         left(200)
+# stars()
+# mainloop()
+
+#modifications 
+def flower():
+    pensize(2)
+    speed(50)
+    for i in range(150):
         circle(190-i,90)
         left(90)
         circle(190-i,90)
-        left(200)
-stars()
+        left(60)
+flower()
 mainloop()
